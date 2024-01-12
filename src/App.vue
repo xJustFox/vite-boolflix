3d89114ccb8c6ed9920fe73a1339cfcd8c746f5f
xJustFox <dodydaversa@gmail.com>
2024-01-12 12:52:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use './style/generals.scss' as *;
+@use './style//partials/variables' as *;
+</style>
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { reactive  } from "vue";
+
+export const store = reactive({
+    apiSearchMovie: 'https://api.themoviedb.org/3/search/movie',
+    // utilizzare il '?/& query=' per ricercare un film specifico tramite nome
+    apiToken: 'api_key=3b5b5dcfa6bfdf7b0d812359db249350',
+})
